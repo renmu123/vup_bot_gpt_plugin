31ee0e408407e3401a9cc0465d52ab8ce55d3448
renmu123 <1101022351@qq.com>
2023-10-09 17:34:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+import Koa from "koa";
+import bodyParser from "es-koa-body";
+import { ChatGPTUnofficialProxyAPI } from "chatgpt";
+import dotenv from "dotenv";
+
+dotenv.config(); // 加载环境变量
+
+const app = new Koa();
+const port = process.env.PORT || 3010;
+
+const accessToken = process.env.ACCESS_TOKEN;
+console.log("ppp", accessToken);
+
+app.use(bodyParser());
+
+const api = new ChatGPTUnofficialProxyAPI({
+  accessToken: accessToken,
+  apiReverseProxyUrl: "https://ai.fakeopen.com/api/conversation",
+});
+
+app.use(async (ctx) => {
+  console.log(ctx.request.body);
+  const { message } = ctx.request.body;
+
+  try {
+    const response = await api.sendMessage(message, {
+      conversationId: "af42aa0f-cd6b-4ba8-814e-c824318d2ed3",
+      parentMessageId: "d001827f-f3a5-45ee-bed8-30441d1b640b",
+    });
+
+    const reply = response.text;
+    ctx.body = { reply };
+  } catch (error) {
+    console.error(error);
+    ctx.status = 500;
+    ctx.body = { error: "Internal Server Error" };
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
