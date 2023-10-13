a6936b5b99459ff48bf66a00f3911e26132a543e
renmu123 <1101022351@qq.com>
2023-10-13 15:43:59 +0800

    增加模式功能

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 import Koa from "koa";
-import bodyParser from "es-koa-body";
+import { koaBody } from "koa-body";
+import Router from "koa-router";
 import { ChatGPTUnofficialProxyAPI } from "chatgpt";
 import dotenv from "dotenv";
 
 dotenv.config(); // 加载环境变量
 
 const app = new Koa();
+const router = new Router();
+
 const port = process.env.PORT || 3010;
-
 const accessToken = process.env.ACCESS_TOKEN;
-
 const DATA = JSON.parse(process.env.DATA);
-
-app.use(bodyParser());
 
 const api = new ChatGPTUnofficialProxyAPI({
   accessToken: accessToken,
   apiReverseProxyUrl: "https://ai.fakeopen.com/api/conversation",
 });
 
-app.use(async (ctx) => {
+router.post("/", koaBody(), async (ctx) => {
+  console.log(ctx, ctx.request.body);
   const { message, mode } = ctx.request.body;
-  console.log(message);
+
   if (DATA.length === 0) {
     ctx.status = 500;
     ctx.body = { reply: "请先进行配置" };
@@ -54,6 +54,11 @@ app.use(async (ctx) => {
   }
 });
 
+router.get("/list", koaBody(), async (ctx) => {
+  ctx.body = DATA.map((item) => item.name);
+});
+
+app.use(router.routes());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
